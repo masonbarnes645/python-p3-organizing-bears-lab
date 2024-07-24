b23970dfe5612853ca6eff8d6ec8c05b0714da17
Mason Barnes <barnesmason980@gmail.com>
2024-07-24 14:02:32 -0700

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO [bears] (id, name, age, sex, color, temperament, alive) 
+VALUES (1, 'Mr. Chocolate', 13, 'F', 'orange', 'nice', TRUE);
+
+INSERT INTO [bears] (id, name, age, sex, color, temperament, alive) 
+VALUES (2,  'Rowdy', 13, 'F', 'orange', 'nice', TRUE);
+
+INSERT INTO [bears] (id, name, age, sex, color, temperament, alive) 
+VALUES (3,  'Tabitha', 13, 'F', 'orange', 'nice', TRUE);
+
+INSERT INTO [bears] (id, name, age, sex, color, temperament, alive) 
+VALUES (4,  'Sergeant Brown', 13, 'F', 'orange', 'nice', TRUE);
+
+INSERT INTO [bears] (id, name, age, sex, color, temperament, alive) 
+VALUES (5,  'Melissa', 13, 'F', 'orange', 'nice', TRUE);
+
+INSERT INTO [bears] (id, name, age, sex, color, temperament, alive) 
+VALUES (6,  'Grinch', 13, 'F', 'orange', 'nice', TRUE);
+
+INSERT INTO [bears] (id, name, age, sex, color, temperament, alive) 
+VALUES (7,  'Wendy', 13, 'F', 'orange', 'nice', TRUE);
+
+INSERT INTO [bears] (id, name, age, sex, color, temperament, alive) 
+VALUES (8,  NULL, 13, 'F', 'orange', 'nice', TRUE);
